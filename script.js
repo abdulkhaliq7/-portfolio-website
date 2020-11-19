2abe5ba3c2e77d98f58d29ef6e3ede4502fcedc2
abdulkhaliq7 <abdikhaliq_7@hotmail.com>
2020-11-19 15:13:59 +0800

    modal

--- script.js
+++ script.js
@@ -37,3 +37,36 @@ function setTheme(mode) {
 
     localStorage.setItem('theme', mode)
 }
+
+
+//? Get modal element
+let modal = document.getElementById("simple-modal");
+//? Get open modal button 
+let modalBtn = document.getElementById("submit-btn")
+//? Get close button
+let closeBtn = document.getElementsByClassName("close-btn")[0]
+
+//? Listen for open click
+modalBtn.addEventListener('click', openModel)
+//? Listen for close click
+closeBtn.addEventListener('click', closeModel)
+//? Listen for outside click
+window.addEventListener('click', outsideClick)
+
+
+//? Function to open modal
+function openModel(){
+    modal.style.display = 'block'
+}
+
+//? Function to close modal
+function closeModel(){
+    modal.style.display = 'none'
+}
+
+//? Function to close modal if ouside click
+function outsideClick(e){
+    if(e.target == modal){
+    modal.style.display = 'none'
+    }
+}
